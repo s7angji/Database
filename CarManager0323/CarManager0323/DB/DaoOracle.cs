72dcebe61fae1deff32085634cf03436b9b5bd86
s7angji <s7angji@gmail.com>
2021-03-23 16:24:29 +0900

    20210323_1624

---
+++ CarManager0323/CarManager0323/DB/DaoOracle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManager0323.DB
+{
+    class DaoOracle
+    {
+    }
+}
---
+++ CarManager0323/CarManager0323/UI/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarManager0323
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
